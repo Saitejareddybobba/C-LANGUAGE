087c236e9e6c9d5d20f90e007131767a973f7ee9
Saitejareddybobba <86121208+Saitejareddybobba@users.noreply.github.com>
2021-06-18 22:45:03 +0530

    created the file
    created a file and used
    *if-else statements
    *welcome to github

---
+++ Example.c
@@ -0,0 +1,16 @@
+// this is the first example to write a code ..
+
+#include<stdio.h>
+
+main(){
+  int n;
+  printf("enter an number::");
+  scanf("%d",&n);
+  
+  if(n==1){
+    printf("welcome to github");
+  }
+  else{
+    printf("wrong input even the welcome to github");
+  }
+}
